4862c1df2bb4398e495a8df4c3b2223298e210c1
umang <iumangsharma001@gmail.com>
2020-07-08 17:06:11 +0530

    Activity added

---
+++ app/src/main/java/com/sharmaumang/TrackBed/Hospital/HospitalLogin.java
@@ -0,0 +1,16 @@
+package com.sharmaumang.TrackBed.Hospital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sharmaumang.TrackBed.R;
+
+public class HospitalLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospital_login);
+    }
+}
